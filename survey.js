2d1217e4f9ea2bdf172768f7ed9629389abc83c2
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-02 23:56:56 +0000

    Add survey that uses the user answers in a sentence

---
+++ survey.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What\'s your name? ', (name) => {
+  rl.question('What\'s an activity you like doing? ', (activity) => {
+    rl.question('What do you listen to while doing that? ', (listen) => {
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)? ', (meal) => {
+        rl.question('What\'s your favourite thing to eat for that meal? ', (food) => {
+          rl.question('Which sport is your absolute favourite? ', (sport) => {
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (superpower) => {
+              console.log(`\n${name} loves listening to ${listen} while ${activity}, devouring ${food} for ${meal}, prefers ${sport} over any other sport, and is amazing at ${superpower}.`);
+          
+              rl.close();
+          
+            });
+          });
+        });
+      });
+    });
+  });
+});
